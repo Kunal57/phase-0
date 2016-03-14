1c093a16ab85b606a3a004c229052382fc7a4d24
Kunal Patel <kunalpatelx92@gmail.com>
2016-03-14 07:40:06 -0500

    Update pad-array reflection

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -1,6 +1,6 @@
 # Pad an Array
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [by myself]
 
 # I spent [] hours on this challenge.
 
@@ -78,26 +78,29 @@ puts pad([1,2,3],5,"apple")
 
 # 4. Reflection
 
-# Were you successful in breaking the problem down into small steps?
+=begin
+1. Were you successful in breaking the problem down into small steps?
 
-# Yes. We broke the problem down into smaller steps and focused on writing code for each step as opposed to trying to write the code for the whole problem. This helps make complicated problems into smaller manageable problems. Pseudocode is a very helpful for problems like these.  
+Yes. We broke the problem down into smaller steps and focused on writing code for each step as opposed to trying to write the code for the whole problem. This helps make complicated problems into smaller manageable problems. Pseudocode is a very helpful for problems like these.  
 
-# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+2. Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
 
-# The pseudocode was great and is defiantly made the transition to code easier. Pseudocode doesn't fully narrate the syntax to be used but it does organize my thought process & makes writing code for each step easier. The difficulties we had were to recognize the correct methods and objects to use in our solution according to our pseudocode.
+The pseudocode was great and is defiantly made the transition to code easier. Pseudocode doesn't fully narrate the syntax to be used but it does organize my thought process & makes writing code for each step easier. The difficulties we had were to recognize the correct methods and objects to use in our solution according to our pseudocode.
 
-# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+3. Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
 
-# Our initial solutions didn't pass the tests. For our pad! method, our code wasn't destructive because we hadn't returned the array at the end of our method. As a result, our return was nil and didn't change the original array at all with our new additions. Our second pad method didn't pass the tests because it was destructive. This was because in the first part of our solution (if) we returned the original array if the minimum size was below the array length. As a result, the original array was technically modified and returned so it was destructive. We fixed the problem by cloning the array to start the method, and returning it instead of the original array. This way, the new cloned array did not destroy the original array.
+Our initial solutions didn't pass the tests. For our pad! method, our code wasn't destructive because we hadn't returned the array at the end of our method. As a result, our return was nil and didn't change the original array at all with our new additions. Our second pad method didn't pass the tests because it was destructive. This was because in the first part of our solution (if) we returned the original array if the minimum size was below the array length. As a result, the original array was technically modified and returned so it was destructive. We fixed the problem by cloning the array to start the method, and returning it instead of the original array. This way, the new cloned array did not destroy the original array.
 
-# When you refactored, did you find any existing methods in Ruby to clean up your code?
-
-# 
-
-# How readable is your solution? Did you and your pair choose descriptive variable names?
+4. When you refactored, did you find any existing methods in Ruby to clean up your code?
 
 # 
 
-# What is the difference between destructive and non-destructive methods in your own words?
+5. How readable is your solution? Did you and your pair choose descriptive variable names?
 
-# Destructive methods are methods that change the input of the method for future use. The input is altered through the process of the method and the output becomes the permanent. This is different than a non-destructive method which doesn't change the input for future use. The input goes into the method, and produces a new output. The difference is that when the original input is displayed, it will be as it was before being processed through the non-destructive method. Basically, a destructive method permanently changes the value while a non-destructive method leaves the original value as is.
+Our solution is very readable. We used indentation and laid the code out in a neat and organized way for both methods. We also chose descriptive names such as 'padding' to denote the padded array and 'clone' to denote the cloned copy of the original array. These are descriptive to the purpose they serve and can help the developer identify the contents of the variable.
+
+6. What is the difference between destructive and non-destructive methods in your own words?
+
+Destructive methods are methods that change the input of the method for future use. The input is altered through the process of the method and the output becomes the permanent. This is different than a non-destructive method which doesn't change the input for future use. The input goes into the method, and produces a new output. The difference is that when the original input is displayed, it will be as it was before being processed through the non-destructive method. Basically, a destructive method permanently changes the value while a non-destructive method leaves the original value as is.
+
+=end
