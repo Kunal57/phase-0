4e8960dbc45eef5e99da75eeefa484eec0cc3124
Kunal Patel <kunalpatelx92@gmail.com>
2016-03-13 22:35:22 -0500

    Add reflection to pad-array solution

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -25,66 +25,79 @@
 # Return the new padded array.
 # End
 
-# 1. Initial Solution
 
+# 1. Initial Solution
 def pad!(array, min_size, value = nil) #destructive
-  if array.length >= min_size
-  	array
-  else array.length < min_size
+ if array.length >= min_size
+   p array
+ else array.length < min_size
    padding = min_size - array.length
    padding.times do |x|
-   array.push(value)
-   end
+    array.push(value)
+    end
   end
+  p array
 end
 
-puts pad!([1,2,3], 5, "apple")
+puts pad!([1,2,3],5)
+puts pad!([1,2,3],5,"apple")
+
 
 def pad(array, min_size, value = nil) #non-destructive
+ clone = array.clone
   if array.length >= min_size
-  	array
-  else array.length <= min_size
-   padding = min_size - array.length
-   padding.times do |x|
-   array.push(value)
-   end
+    p clone
+  else
+    new = array.clone
+    padding = min_size - array.length
+    padding.times do |x|
+    new.push(value)
+    end
+    return new
   end
 end
+
+puts pad([1,2,3],5)
+puts pad([1,2,3],5,"apple")
 
 # 3. Refactored Solution
 
+# Method that was not destructive. Need to figure out why this one is non-destructive and the first method is destructive.
+
+# def pad!(array, min_size, value = nil) #destructive
+#   if array.length >= min_size
+#     array
+#   else array.length < min_size
+#    padding = min_size - array.length
+#    padding.times do |x|
+#    array.push(value)
+#    end
+#   end
+# end
 
 
 # 4. Reflection
 
+# Were you successful in breaking the problem down into small steps?
 
+# Yes. We broke the problem down into smaller steps and focused on writing code for each step as opposed to trying to write the code for the whole problem. This helps make complicated problems into smaller manageable problems. Pseudocode is a very helpful for problems like these.  
 
-# # 1. Initial Solution
-# def pad!(array, min_size, value = nil) #destructive
-#  if array.length >= min_size
-#    p array
-#  else array.length < min_size
-#    padding = min_size - array.length
-#    padding.times do |x|
-#    	array.push(value)
-#    end
-#  end
-#  p array
-# end
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
 
-# pad!([1,2,3],5)
-# pad!([1,2,3],5,"apple")
+# The pseudocode was great and is defiantly made the transition to code easier. Pseudocode doesn't fully narrate the syntax to be used but it does organize my thought process & makes writing code for each step easier. The difficulties we had were to recognize the correct methods and objects to use in our solution according to our pseudocode.
 
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
 
-# def pad(array, min_size, value = nil) #non-destructive
-#  pad_size = min_size - array.length
-#  nondes = Array.new(pad_size.to_i, value)
-#  if array.length >= min_size || min_size == 0
-#    return array
-#  else
-#    array + nondes
-#  end
-# end
+# Our initial solutions didn't pass the tests. For our pad! method, our code wasn't destructive because we hadn't returned the array at the end of our method. As a result, our return was nil and didn't change the original array at all with our new additions. Our second pad method didn't pass the tests because it was destructive. This was because in the first part of our solution (if) we returned the original array if the minimum size was below the array length. As a result, the original array was technically modified and returned so it was destructive. We fixed the problem by cloning the array to start the method, and returning it instead of the original array. This way, the new cloned array did not destroy the original array.
 
-# pad([1,2,3],5)
-# pad([1,2,3],5,"apple")
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
+
+# 
+
+# How readable is your solution? Did you and your pair choose descriptive variable names?
+
+# 
+
+# What is the difference between destructive and non-destructive methods in your own words?
+
+# Destructive methods are methods that change the input of the method for future use. The input is altered through the process of the method and the output becomes the permanent. This is different than a non-destructive method which doesn't change the input for future use. The input goes into the method, and produces a new output. The difference is that when the original input is displayed, it will be as it was before being processed through the non-destructive method. Basically, a destructive method permanently changes the value while a non-destructive method leaves the original value as is.
