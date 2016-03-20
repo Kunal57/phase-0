ae7e7b822ccf5a4ed5d5fa708cd758cc1f4a0134
Kunal Patel <kunalpatelx92@gmail.com>
2016-03-19 19:11:03 -0500

    Add nested_array reflection

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -71,3 +71,24 @@ startup_names.map! do|x|
   end
 end
 p startup_names
+
+# Reflection
+
+=begin
+
+1. What are some general rules you can apply to nested arrays?
+
+- The deeper you want to dig into the dimensions of an array, the more indices you need to indicate. For example, for a two-dimensional array, you need two indices to select the element (array[1][3]).
+-The nested array is just another object within the array.
+-Iteration through a nested array needs to be signified with code to run through the nested array.
+
+2. What are some ways you can iterate over nested arrays?
+
+- In release 3, we iterated over the nested array by asking if the object was an array. If the object was an array, we iterated through the array and added 5 to each number in the array. Else, if the object wasn't an array, we simply added 5 to the number.
+-Another was to iterate over an array is to iterate each object in the array and then use an iteration within the iteration. So when iterating each object, you would iterate each objects object if they had any. This would iterate over the array in the array to produce the desired results.
+
+3. Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+I learned about the .kind_of? method. This method tests to see if the value is an array and runs the code block if it is. Some methods that we re-used were .map to iterate though each value and process the code block associated with it. We decided that .map was a good method because it iterated through the values and displayed them correctly. The .each method executes the block for each element and returns the array itself. The .map method executes the block of code for each element but returns a new array. We felt like the return of a new array would be better for our results.
+
+=end
