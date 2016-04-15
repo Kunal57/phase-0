b814334cc05e0bdbbe7ae562c7abecbee8cab26e
Kunal Patel <kunalpatelx92@gmail.com>
2016-04-15 18:48:03 -0500

    Complete Challenge 4.6.5

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -1,12 +1,28 @@
-# Concatenate Two Arrays
+# 4.6.5 Concatenate Two Arrays
 
-# I worked on this challenge [by myself, with: ].
+=begin
 
+PSEUDOCODE
+Input: 2 Arrays
+Output: 1 Array
+Steps:
+1. Define array_concat method that takes two arrays as parameters
+2. Add array1 to array2
+3. End method
 
-# Your Solution Below
+=end
 
-def array_concat(array_1, array_2)
-  return array_1 + array_2
+# Initial Solution
+
+def array_concat(array1, array2)
+	array1 + array2
 end
 
-puts array_concat([1,2,3,4],[5,6,7,8])
+# DRIVER CODE
+p array_concat([1,2,3],[4,5,6])
+
+# Refactor
+
+def array_concat(array1, array2)
+	array1.concat(array2)
+end
