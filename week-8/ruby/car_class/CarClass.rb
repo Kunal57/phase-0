1786a7dc8d4c2721ac5c61c739a87833ecdf1164
Kunal Patel <kunalpatelx92@gmail.com>
2016-05-11 15:15:31 -0500

    Complete CarClass Challenge

--- week-8/ruby/car_class/CarClass.rb
+++ week-8/ruby/car_class/CarClass.rb
@@ -19,6 +19,44 @@ Steps:
 
 # Initial Solution
 
+class Pizza
+	attr_reader :customer_name
+
+	def initialize (customer_name)
+		@customer_name = customer_name
+		@size = "large"
+		@crust = "pan"
+		@toppings = ["cheese"]
+		puts "#{@customer_name} - the following are the sizes, crusts, and toppings we offer:"
+		puts "Sizes: large, medium, & small"
+		puts "Crusts: pan, thin, & deep dish"
+		puts "Toppings: Mushrooms, Olives, Pineapple, Green Peppers, Tomatoes, & Jalapenos"
+	end
+
+	def pizza_size(size)
+		@size = size
+	end
+
+	def pizza_crust(crust)
+		@crust = crust
+	end
+
+	def add_toppings(*topping)
+		@toppings.concat(topping)
+	end
+
+	def review_order
+		puts "#{@customer_name} has ordered a #{@size} pizza with a #{@crust} crust that has #{@toppings.length} toppings!"
+	end
+
+	def review_toppings
+		puts "#{@customer_name} has the following toppings on their pizza:"
+		@toppings.each do |topping|
+			puts "#{topping}"
+		end
+	end
+end
+
 class Car
 	attr_reader
 	attr_accessor :model, :color
@@ -58,85 +96,58 @@ class Car
 
 	def load_pizza(pizza)
 		@pizzas << pizza
-		"You have loaded the #{pizza} pizza into your car!"
+		"You have loaded #{pizza.customer_name}'s pizza into your car!"
 	end
 
 	def check_pizza
-		@pizzas
+		@pizzas.each do |pizza|
+			puts "#{pizza.customer_name}'s pizza is read to be delivered!"
+		end
 	end
 
 	def deliver_pizza
 		pizza_delivery = @pizzas.delete_at(0)
-		"You have deliveried the #{pizza_delivery} pizza!"
+		"You have deliveried #{pizza_delivery.customer_name}'s pizza!"
 	end
 end
 
-class Pizza
-	def initialize (customer_name)
-		@customer_name = customer_name
-		@size = "large"
-		@crust = "pan"
-		@toppings = ["cheese"]
-		puts "#{@customer_name} - the following are the sizes, crusts, and toppings we offer:"
-		puts "Sizes: large, medium, & small"
-		puts "Crusts: pan, thin, & deep dish"
-		puts "Toppings: Mushrooms, Olives, Pineapple, Green Peppers, Tomatoes, & Jalapenos"
-	end
-
-	def pizza_size(size)
-		@size = size
-	end
-
-	def pizza_crust(crust)
-		@crust = crust
-	end
-
-	def add_toppings(*topping)
-		@toppings.concat(topping)
-	end
-
-	def review_order
-		puts "#{@customer_name} has ordered a #{@size} pizza with a #{@crust} crust that has #{@toppings.length} toppings!"
-	end
-
-	def review_toppings
-		puts "#{@customer_name} has the following toppings on their pizza:"
-		@toppings.each do |topping|
-			puts "#{topping}"
-		end
-	end
-end
 
 
 
 # DRIVER CODE
 
-# tesla = Car.new("Tesla Model S","grey")
-# p tesla.direction(0.25,25)
-# p tesla.stop
-# p tesla.turn("right")
-# p tesla.direction(1.5,35)
-# p tesla.speed
-# p tesla.speed(15)
-# p tesla.direction(0.25)
-# p tesla.stop
-# p tesla.turn("left")
-# p tesla.direction(1.4,35)
-# p tesla.stop
-# p tesla.total_distance
-# p tesla.load_pizza("Veggie")
-# p tesla.load_pizza("Cheese")
-# p tesla.load_pizza("Mushroom")
-# p tesla.check_pizza
-# p tesla.deliver_pizza
-# p tesla.check_pizza
+kunals_pizza = Pizza.new("Kunal Patel")
+kunals_pizza.review_order
+kunals_pizza.add_toppings("mushrooms","peppers","olives")
+kunals_pizza.review_order
+kunals_pizza.pizza_size("small")
+kunals_pizza.pizza_crust("thin")
+kunals_pizza.add_toppings("pineapple")
+kunals_pizza.review_order
+kunals_pizza.review_toppings
 
-cheese_pizza = Pizza.new("Kunal Patel")
-cheese_pizza.review_order
-cheese_pizza.add_toppings("mushrooms","peppers","olives")
-cheese_pizza.review_order
-cheese_pizza.pizza_size("small")
-cheese_pizza.pizza_crust("thin")
-cheese_pizza.add_toppings("pineapple")
-cheese_pizza.review_order
-cheese_pizza.review_toppings
+elons_pizza = Pizza.new("Elon Musk")
+elons_pizza.add_toppings("mushrooms", "jalapenos","olives")
+elons_pizza.review_toppings
+elons_pizza.review_order
+
+tesla = Car.new("Tesla Model S","grey")
+p tesla.direction(0.25,25)
+p tesla.stop
+p tesla.turn("right")
+p tesla.direction(1.5,35)
+p tesla.speed
+p tesla.speed(15)
+p tesla.direction(0.25)
+p tesla.stop
+p tesla.turn("left")
+p tesla.direction(1.4,35)
+p tesla.stop
+p tesla.total_distance
+p tesla.load_pizza(kunals_pizza)
+p tesla.load_pizza(elons_pizza)
+p tesla.check_pizza
+p tesla.deliver_pizza
+p tesla.check_pizza
+
+
